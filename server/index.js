1aac35459e574700e614ff352dfc0e5e03a0ad4e
abeGizaw <gizawabe3@gmail.com>
2023-11-22 22:05:23 -0500

    creating first table

--- server/index.js
+++ server/index.js
@@ -1,6 +1,10 @@
-import express from 'express'
-const app = express()
+const express = require("express");
+const db = require("./models");
 
-app.listen(3001, () => {
-	console.log("Server running on port 3001");
+const app = express();
+
+db.sequelize.sync().then(() => {
+  app.listen(3001, () => {
+    console.log("Server running on port 3001");
+  });
 });
